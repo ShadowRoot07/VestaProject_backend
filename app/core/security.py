9381776ea9ff6f05bcb82084b6debfe84bf3d4de
mr-papu-jpg <angstart.1109@gmail.com>
2026-02-02 08:18:13 -0400

    fix: Fix a silly Python tabulation error

--- app/core/security.py
+++ app/core/security.py
@@ -4,7 +4,7 @@ import jose.jwt
 from sqlmodel import Session, select
 from app.database import get_session
 from app.models.users import User
-from .security_config import SECRET_KEY, ALGORITHM 
+from .security_config import SECRET_KEY, ALGORITHM
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
@@ -21,14 +21,16 @@ def get_current_user(
         payload = jose.jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
-    except jose.JWTError:
+            # Aquí faltaba la instrucción que causaba el error
+            raise credentials_exception
+    except jose.jwt.JWTError:
         raise credentials_exception
 
     user = session.exec(select(User).where(User.username == username)).first()
-    
+
     if user is None:
         raise credentials_exception
-    
+
     return user
 
 def get_current_admin_user(current_user: User = Depends(get_current_user)):
